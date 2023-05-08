052f70cf3a423b6a170f441fdb85a8889926c5f3
Ricci-Liu <rl4800@nyu.edu>
2023-05-08 20:21:33 +0800

    hh

--- Examples/Ruiqi-Project-B/localValues.js
+++
@@ -1,8 +0,0 @@
-localStorage.setItem('2start', false);
-localStorage.setItem('3start', false);
-localStorage.setItem('4start', false);
-localStorage.setItem('5start', false);
-localStorage.setItem('6start', false);
-localStorage.setItem('7start', false);
-localStorage.setItem('8start', false);
-localStorage.setItem('9start', false);
